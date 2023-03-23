6343abf0034b7d7f86d0a5e59e94ee838d316a3d
JoseGabino12 <gabinoMorales1212@hotmail.com>
2023-03-22 21:11:26 -0600

    Adding styles and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-import { Article, Brand, CTA } from './components'
+import {
+  Footer,
+  Blog,
+  Header,
+  Possibility,
+  Features,
+  WhatGPT3
+} from './containers'
+
+import { CTA, Brand, Navbar } from './components'
+
+import './App.css'
 
 const App = () => {
-  return <div>GPT-3</div>
+  return (
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+
 import './navbar.css'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='gpt3_navbar'>
+      <div className='gpt3_navbar_links'>
+        <div className='gpt3_navbar_links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
