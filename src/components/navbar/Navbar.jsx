291a1609da42ff98c4b49653778486645027187b
JoseGabino12 <gabinoMorales1212@hotmail.com>
2023-03-22 22:31:58 -0600

    Navbar component created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
-
+import { Menu } from './components/Menu'
 import './navbar.css'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <div className='gpt3_navbar'>
       <div className='gpt3_navbar_links'>
         <div className='gpt3_navbar_links_logo'>
           <img src={logo} alt='logo' />
         </div>
+        <div className='gpt3_navbar_links_container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='gpt3_navbar_sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign in</button>
+      </div>
+      <div className='gpt3_navbar_menu'>
+        {toggle ? (
+          <RiCloseLine
+            color='#fff'
+            size={27}
+            onClick={() => setToggle(false)}
+          />
+        ) : (
+          <RiMenu3Line color='#fff' size={27} onClick={() => setToggle(true)} />
+        )}
+
+        {toggle && (
+          <div className='gpt3_navbar_menu_container scale_up_center'>
+            <div className='gpt3_navbar_menu_container_links'>
+              <Menu />
+              <div className='gpt3_navbar_menu_container_links_sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign in</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
---
+++ src/components/navbar/components/Menu.jsx
@@ -0,0 +1,21 @@
+export const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href='#home'>Home</a>
+      </p>
+      <p>
+        <a href='#gpt3'>What is GPT3?</a>
+      </p>
+      <p>
+        <a href='#possibility'>Open AI</a>
+      </p>
+      <p>
+        <a href='#features'>Case Studies</a>
+      </p>
+      <p>
+        <a href='#blog'>Library</a>
+      </p>
+    </>
+  )
+}
